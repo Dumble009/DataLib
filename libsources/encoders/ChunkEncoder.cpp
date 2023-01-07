e17539ad8f2b0a2e5c5f57ae6cdd249930bfd121
Dumble009 <daichi19990821@gmail.com>
2023-01-07 22:17:27 +0900

    GTestを導入。ビルドと同時にテストが走るようにtask.jsonを設定

--- libsources/encoders/ChunkEncoder.cpp
+++ libsources/encoders/ChunkEncoder.cpp
@@ -1,1 +1,12 @@
-﻿
+﻿#include "ChunkEncoder.h"
+
+namespace data
+{
+    namespace encoder
+    {
+        ChunkEncoder::ChunkEncoder()
+        {
+            writeData = std::vector<char>();
+        }
+    }
+}
--- libsources/encoders/ChunkEncoder.h
+++ libsources/encoders/ChunkEncoder.h
@@ -1,9 +1,31 @@
 ﻿#pragma once
+#include <vector>
+#include <string>
+#include "../DataLibErrorCode.h"
 
 namespace data
 {
-    template <class T>
-    class ChunkEncoder
+    namespace encoder
     {
-    };
+        // T型の値をheadからsize個、pathのファイルに書き込む
+        class ChunkEncoder
+        {
+        private:
+            std::vector<char> writeData;
+            DataLibErrorCode Write();
+
+        public:
+            ChunkEncoder();
+
+            template <class T>
+            DataLibErrorCode Encode(
+                const T *head,
+                size_t size,
+                const std::string &path,
+                int structure)
+            {
+                return DataLibErrorCode::DATALIB_SUCCESS;
+            }
+        };
+    }
 }
