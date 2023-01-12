c3eef6a3b93df949b5965fd92665e1246f37c9fc
Dumble009 <daichi19990821@gmail.com>
2023-01-12 21:18:51 +0900

    decodersフォルダを作成し、中にDecoderクラスの定義とCMakeLists.txtを作成。

---
+++ libsources/decoders/Decoder.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include <vector>
+#include <string>
+
+#include "../DataLibErrorCode.h"
+
+namespace data
+{
+    namespace decoder
+    {
+        class Decoder
+        {
+        private:
+            std::vector<char> readBytes; // ファイルから読み込んだバイト配列
+
+            template <class T>
+            DataLibErrorCode DecodeImp(T *data,
+                                       const std::vector<char> &bytes,
+                                       int *head,
+                                       int option);
+
+            template <class T>
+            DataLibErrorCode DecodeImp(std::vector<T> *data,
+                                       const std::vector<char> &bytes,
+                                       int *head,
+                                       int option);
+
+        public:
+            Decoder();
+
+            DataLibErrorCode Read(const std::string &path);
+
+            template <class T>
+            DataLibErrorCode Decode(T *data,
+                                    const std::string &path,
+                                    int option);
+        };
+    }
+}
