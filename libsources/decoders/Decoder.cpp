0c77d23518c6de2bfe6ea5a8ceb8b97ef79a1b5a
Dumble009 <daichi19990821@gmail.com>
2023-01-12 22:01:07 +0900

    Decoderクラスの中身を実装。まだ動作確認は行っていない。

--- libsources/decoders/Decoder.cpp
+++ libsources/decoders/Decoder.cpp
@@ -1,1 +1,41 @@
-﻿
+﻿#include "Decoder.h"
+
+#include <fstream>
+
+namespace data
+{
+    namespace decoder
+    {
+        Decoder::Decoder() : readBytes(){};
+
+        DataLibErrorCode Decoder::Read(const std::string &path)
+        {
+            // ファイルサイズを取得するために、読み取り位置を末尾にする
+            std::ifstream ifs(path, std::ios::in | std::ios::binary | std::ios::ate);
+
+            // ファイルのオープンに失敗したらエラーを返す
+            if (!ifs)
+            {
+                return DataLibErrorCode::DATA_LIB_FAILED_TO_OPEN_FILE;
+            }
+
+            try
+            {
+                // ファイルを開く際に読み取り位置を末尾にしているので、
+                // 現在の読み取り位置=ファイルサイズになっている
+                size_t byteCounts = static_cast<size_t>(ifs.tellg());
+
+                ifs.seekg(0, std::ifstream::beg); // 読み取り位置を先頭に移動
+
+                readBytes.resize(byteCounts);
+                ifs.read(readBytes.begin()._Ptr, byteCounts);
+            }
+            catch (...)
+            {
+                return DataLibErrorCode::DATA_LIB_FAILED_TO_READ_DATA;
+            }
+
+            return DataLibErrorCode::DATA_LIB_SUCCESS;
+        }
+    }
+}
--- libsources/decoders/Decoder.h
+++ libsources/decoders/Decoder.h
@@ -35,5 +35,90 @@ namespace data
                                     const std::string &path,
                                     int option);
         };
+
+        template <class T>
+        DataLibErrorCode Decoder::DecodeImp(T *data,
+                                            const std::vector<char> &bytes,
+                                            int *head,
+                                            int option)
+        {
+            try
+            {
+                size_t size = sizeof(T);
+
+                // dataのポインタをcharのポインタに変換
+                // VC++のreinterpret_castにはバグがある？ようで上手く動かないので使わない
+                auto bytePtr = (char *)(void *)data;
+
+                for (int i = 0; i < size; i++)
+                {
+                    bytePtr[i] = bytes[*head + i];
+                }
+
+                *head += size;
+
+                return DataLibErrorCode::DATA_LIB_SUCCESS;
+            }
+            catch (...)
+            {
+                return DataLibErrorCode::DATA_LIB_FAILED_TO_DECODE;
+            }
+        }
+
+        template <class T>
+        DataLibErrorCode Decoder::DecodeImp(std::vector<T> *data,
+                                            const std::vector<char> &bytes,
+                                            int *head,
+                                            int option)
+        {
+            try
+            {
+                // まずはベクタのサイズを取り出す。
+                size_t dataSize = 0;
+                auto ret = DecodeImp(&dataSize, bytes, head, option);
+                if (!IsDataLibActionSucceeded(ret))
+                {
+                    return ret;
+                }
+
+                // 取得したデータ数の領域を確保し、そこに順番に値を入れていく
+                data->resize(dataSize);
+                for (auto itr = data->begin(); itr != data->end(); itr++)
+                {
+                    ret = DecodeImp(itr._Ptr, bytes, head, option);
+                    if (!IsDataLibActionSucceeded(ret))
+                    {
+                        return ret;
+                    }
+                }
+
+                return DataLibErrorCode::DATA_LIB_SUCCESS;
+            }
+            catch (...)
+            {
+                return DataLibErrorCode::DATA_LIB_FAILED_TO_DECODE;
+            }
+        }
+
+        template <class T>
+        DataLibErrorCode Decoder::Decode(T *data,
+                                         const std::string &path,
+                                         int option)
+        {
+            // まずはpathが示すファイルからreadBytesにバイト列を読み込む
+            auto ret = Read(path);
+            if (!IsDataLibActionSucceeded(ret))
+            {
+                return ret;
+            }
+
+            ret = DecodeImp(data, readBytes, option);
+            if (!IsDataLibActionSucceeded(ret))
+            {
+                return ret;
+            }
+
+            return DataLibErrorCode::DATA_LIB_SUCCESS;
+        }
     }
 }
