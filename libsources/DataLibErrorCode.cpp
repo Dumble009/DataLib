f1301fd4b45714d196e1af1affb689b2e48fc4ab
Dumble009 <daichi19990821@gmail.com>
2023-01-12 21:29:48 +0900

    データをエンコードする際に、先頭にメタデータを配置するように変更。また、DataLibErrorCodeが正常終了した値になっているかを判定するための関数を追加

---
+++ libsources/DataLibErrorCode.cpp
@@ -0,0 +1,9 @@
+﻿#include "DataLibErrorCode.h"
+
+namespace data
+{
+    inline bool IsDataLibActionSucceeded(DataLibErrorCode code)
+    {
+        return code == DataLibErrorCode::DATA_LIB_SUCCESS;
+    }
+}
--- libsources/DataLibErrorCode.h
+++ libsources/DataLibErrorCode.h
@@ -7,4 +7,7 @@
         DATA_LIB_FAILED_TO_OPEN_FILE,
         DATA_LIB_FAILED_TO_WRITE_DATA,
     };
+
+    // DataLibErrorCodeを返す関数の結果を受け取り、正常に終了したかどうか判定する
+    inline bool IsDataLibActionSucceeded(DataLibErrorCode code);
 }
