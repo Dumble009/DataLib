3ebe5d099de90e98a16bfe5c6ce6d8582232b104
Dumble009 <daichi19990821@gmail.com>
2023-01-13 12:12:31 +0900

    DataLibErrorCodeを文字列に変換するための関数ConvertDataLibErrorCodeToStringと、その結果を確認するためのテストケースを追加

--- libsources/DataLibErrorCode.h
+++ libsources/DataLibErrorCode.h
@@ -1,15 +1,20 @@
-﻿namespace data
+﻿#include <string>
+
+namespace data
 {
-    enum class DataLibErrorCode
+
+    enum class DataLibErrorCode : int
     {
-        DATA_LIB_SUCCESS,
-        DATA_LIB_FAILED_TO_ENCODE,
-        DATA_LIB_FAILED_TO_DECODE,
-        DATA_LIB_FAILED_TO_DECODE_VECTOR,
-        DATA_LIB_FAILED_TO_OPEN_FILE,
-        DATA_LIB_FAILED_TO_WRITE_DATA,
-        DATA_LIB_FAILED_TO_READ_DATA,
+        DATA_LIB_SUCCESS = 0,
+        DATA_LIB_FAILED_TO_ENCODE = 1,
+        DATA_LIB_FAILED_TO_DECODE = 2,
+        DATA_LIB_FAILED_TO_DECODE_VECTOR = 3,
+        DATA_LIB_FAILED_TO_OPEN_FILE = 4,
+        DATA_LIB_FAILED_TO_WRITE_DATA = 5,
+        DATA_LIB_FAILED_TO_READ_DATA = 6,
     };
+
+    std::string ConvertDataLibErrorCodeToString(DataLibErrorCode code);
 
     // DataLibErrorCodeを返す関数の結果を受け取り、正常に終了したかどうか判定する
     inline bool IsDataLibActionSucceeded(DataLibErrorCode code)
---
+++ tests/sources/DataLibErrorCodeTest.cpp
@@ -0,0 +1,31 @@
+﻿#include <gtest/gtest.h>
+
+#include <string>
+
+#include "DataLibErrorCode.h"
+
+void CheckErrorCodeString(data::DataLibErrorCode code,
+                          const std::string &ans)
+{
+    std::string result = data::ConvertDataLibErrorCodeToString(code);
+    ASSERT_EQ(result, ans);
+}
+
+// DataLibErrorCodeを文字列に変換した結果が正しいかどうかチェックする
+TEST(ErrorCodeToStringTest, BasicAssertions)
+{
+    CheckErrorCodeString(data::DataLibErrorCode::DATA_LIB_SUCCESS,
+                         "DATA_LIB_SUCCESS");
+    CheckErrorCodeString(data::DataLibErrorCode::DATA_LIB_FAILED_TO_ENCODE,
+                         "DATA_LIB_FAILED_TO_ENCODE");
+    CheckErrorCodeString(data::DataLibErrorCode::DATA_LIB_FAILED_TO_DECODE,
+                         "DATA_LIB_FAILED_TO_DECODE");
+    CheckErrorCodeString(data::DataLibErrorCode::DATA_LIB_FAILED_TO_DECODE_VECTOR,
+                         "DATA_LIB_FAILED_TO_DECODE_VECTOR");
+    CheckErrorCodeString(data::DataLibErrorCode::DATA_LIB_FAILED_TO_OPEN_FILE,
+                         "DATA_LIB_FAILED_TO_OPEN_FILE");
+    CheckErrorCodeString(data::DataLibErrorCode::DATA_LIB_FAILED_TO_WRITE_DATA,
+                         "DATA_LIB_FAILED_TO_WRITE_DATA");
+    CheckErrorCodeString(data::DataLibErrorCode::DATA_LIB_FAILED_TO_READ_DATA,
+                         "DATA_LIB_FAILED_TO_READ_DATA");
+}
