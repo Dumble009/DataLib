6f1b258f6d6925e183365fc140a4dcf4e6aa1541
Dumble009 <daichi19990821@gmail.com>
2023-01-20 22:01:49 +0900

    gitがフォルダ名の大文字小文字の変化を無視するような設定になっていたので、設定を変更した。

---
+++ libsources/Encoders/Encoder.h
@@ -0,0 +1,119 @@
+﻿#pragma once
+#include <vector>
+#include <string>
+#include "../DataLibErrorCode.h"
+
+namespace data
+{
+    namespace encoder
+    {
+        class Encoder
+        {
+        private:
+            std::vector<char> internalBytes; // エンコードしたデータを保存しておくための配列
+
+            // vectorのエンコーダ
+            template <class T>
+            DataLibErrorCode EncodeImp(const std::vector<T> &data,
+                                       std::vector<char> &bytes,
+                                       int option);
+
+            // 任意の型のエンコーダ
+            template <class T>
+            DataLibErrorCode EncodeImp(const T &data,
+                                       std::vector<char> &bytes,
+                                       int option);
+
+            // internalBytesをファイルに出力する
+            DataLibErrorCode Write(const std::string &path);
+
+        public:
+            Encoder();
+
+            template <class T>
+            DataLibErrorCode Encode(const T &data,
+                                    const std::string &path,
+                                    int option);
+        };
+
+        template <class T>
+        DataLibErrorCode Encoder::EncodeImp(const std::vector<T> &data,
+                                            std::vector<char> &bytes,
+                                            int option)
+        {
+            // コンテナクラスをエンコードする時は、まず要素数を書き込む
+            auto size = data.size();
+            EncodeImp(size, bytes, option);
+
+            // 要素数を書き込んだ後、各要素を順番に書き込んでいく
+            for (const auto &elem : data)
+            {
+                auto ret = EncodeImp(elem, bytes, option);
+
+                // 途中でエンコードに失敗した場合はその時点でリターン
+                if (!IsDataLibActionSucceeded(ret))
+                {
+                    return ret;
+                }
+            }
+
+            return DataLibErrorCode::DATA_LIB_SUCCESS;
+        }
+
+        template <class T>
+        DataLibErrorCode Encoder::EncodeImp(const T &data,
+                                            std::vector<char> &bytes,
+                                            int option)
+        {
+            try
+            {
+                auto sizeT = sizeof(T);
+
+                // dataのポインタをcharのポインタに読み替え
+                // reinterpret_castはVC++ではバグがあるらしい？上手く行かない時がある
+                auto bytePtr = (char *)(void *)(&data);
+
+                for (size_t i = 0; i < sizeT; i++)
+                {
+                    bytes.push_back(bytePtr[i]);
+                }
+
+                return DataLibErrorCode::DATA_LIB_SUCCESS;
+            }
+            catch (...)
+            {
+                // 何らかの例外が発生した場合はエラーコードを投げる。
+                // 例外の内容によって細分化した方がいいかも
+                return DataLibErrorCode::DATA_LIB_FAILED_TO_ENCODE;
+            }
+        }
+
+        template <class T>
+        DataLibErrorCode Encoder::Encode(const T &data,
+                                         const std::string &path,
+                                         int option)
+        {
+            // データ本体の前にメタデータとしてoptionの値を書き込む。
+            auto ret = EncodeImp(option, internalBytes, option);
+            if (!IsDataLibActionSucceeded(ret))
+            {
+                return ret;
+            }
+
+            ret = EncodeImp(data, internalBytes, option);
+            if (!IsDataLibActionSucceeded(ret))
+            {
+                return ret;
+            }
+
+            // エンコードが完了したら得られたバイト列をファイルに書き込む
+            ret = Write(path);
+            if (!IsDataLibActionSucceeded(ret))
+            {
+                return ret;
+            }
+
+            return DataLibErrorCode::DATA_LIB_SUCCESS;
+        }
+    }
+}
